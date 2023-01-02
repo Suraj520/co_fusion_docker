a5bf9111771c6fa4d4c425e4d3e290ded881a5b8
Suraj <suraj.suraj.4.4.1996@gmail.com>
2023-01-03 02:58:48 +0530

    Add files via upload

---
+++ opendocker.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]; then
+  echo 'opendocker.sh requires at least 1 argument.'
+  echo ''
+  echo 'Usage: ./opendocker.sh <IMAGE_NAME>'
+  exit 1
+fi
+
+docker run -it --rm \
+  --net host \
+  --privileged \
+  --gpus all \
+  -e DISPLAY=$DISPLAY \
+  -v $HOME/.X11-unix:/tmp/.X11-unix \
+  --mount type=bind,src=/dev,dst=/dev \
+  --mount type=bind,src=$(pwd)/workspace,dst=/workspace/ \
+  $1 \
+  bash
